8e48fd5203ab5ea6bb513db1eb66e6a40d5f63ed
Ikamjot Hundal <ihundal@my.centennialcollege.ca>
2021-09-29 22:54:03 -0400

    Added Level 2

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    [SerializeField] private bool moving;
+
+    [SerializeField] private Vector2 velocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,14 +10,18 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float groundCheckRadius = 0.15f;
     [SerializeField] private Transform groundCheckPos;
     [SerializeField] private LayerMask whatisGround;
+
     //variables 
     private Rigidbody2D rBody;
-    private bool isGrounded = false; 
+    private Animator anim;
+    private bool isGrounded = false;
+    private bool isFacingRight = true;
 
     // Start is called before the first frame update
     void Start()
     {
         rBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -34,10 +38,32 @@ public class PlayerController : MonoBehaviour
         }
 
         rBody.velocity = new Vector2(horiz * speed, rBody.velocity.y);
+
+        // Check if the sprite need to be flipped
+
+        if(isFacingRight && rBody.velocity.x < 0 || !isFacingRight && rBody.velocity.x > 0)
+        {
+            Flip();
+        }
+
+        // Communicate with the animator 
+        anim.SetFloat("xVelocity", Mathf.Abs(rBody.velocity.x));
+        anim.SetFloat("yVelocity", rBody.velocity.y);
+        anim.SetBool("isGrounded", isGrounded);
     }
 
     private bool GroundCheck()
     {
         return Physics2D.OverlapCircle(groundCheckPos.position, groundCheckRadius, whatisGround); 
+    }
+
+
+    private void Flip()
+    {
+        Vector3 temp = transform.localScale;
+        temp.x *= -1;
+        transform.localScale = temp;
+
+        isFacingRight = !isFacingRight;
     }
 }
